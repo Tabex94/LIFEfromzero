8bf9e1228c77e48272916178eb772ee927de3978
Tabex94 <113391047+Tabex94@users.noreply.github.com>
2022-10-04 23:25:50 -0500

    counting family ready
    next I Have to make the cast for all members of the array

--- gameindex.js
+++ gameindex.js
@@ -7,12 +7,23 @@ initialSetUp=[0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 
 //console.log(initialSetUp);
 
 function countfamily(arr_i){
+    let famcount=0;
+    for(let j=0; j<arr_i.length; j++){
+        if(arr_i[j]==1)
+        {
+            famcount++;
+        }
+    }
+
+    return famcount;
 
 }
 
 function LifeTurn(arr){
 let currentGen=arr;
 let count=0;
+
+/* //FIRST COUNT A SINGLE ELEMENT
 for(let i=0; i<currentGen.length; i++){
 
     //We have to count alive neighbors
@@ -20,10 +31,13 @@ for(let i=0; i<currentGen.length; i++){
     {
         count++;
     }
-}
+}*/
 
 console.log(count);
 return currentGen;
 }
 
-console.log(LifeTurn(initialSetUp));
+//console.log(LifeTurn(initialSetUp));
+
+console.log(countfamily(initialSetUp));
+
